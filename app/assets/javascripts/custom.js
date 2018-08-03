8e3001ec0830c16dc87b817013762d9a9ff49630
DanhManhNguyen <nguyendanhmanh_t61@hus.edu.vn>
2018-08-03 16:03:03 +0700

    Fix layout 1

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,4 +1,5 @@
-$(document).ready(function () {
+$(document).on('turbolinks:load', function() {
+  $(".flash" ).delay(2000).slideUp(1000);
   curr_people = parseFloat($("#current-people").text());
   no_ingredients = parseInt($(".no_ingredients").text());
   static_amounts = [];
