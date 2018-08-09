4ad6f44fcea2725ed5357e1654cd2c2280b6b31a
DanhManhNguyen <nguyendanhmanh_t61@hus.edu.vn>
2018-08-09 10:06:22 +0700

    Delete recipe & bookmark page

--- app/models/favor.rb
+++ app/models/favor.rb
@@ -6,4 +6,5 @@ class Favor < ApplicationRecord
 
   scope :rela, ->(user_id, targetable){where user_id: user_id, targetable: targetable, status: 1}
   scope :rela_bookmark, ->(user_id, targetable){where user_id: user_id, targetable: targetable, status: 2}
+  scope :bookmark_list, ->(user_id){where(user_id: user_id, status: 2).pluck(:targetable_id)}
 end
